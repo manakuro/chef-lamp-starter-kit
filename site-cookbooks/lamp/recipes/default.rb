b4637d6ba9c8fb31f85c6430d091fe52f01105d6
manakuro <a7108100@gmail.com>
2015-05-21 12:54:53 +0900

    Update MySQL5.5 to MySQL5.6

--- site-cookbooks/lamp/recipes/default.rb
+++ site-cookbooks/lamp/recipes/default.rb
@@ -7,13 +7,37 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# PHP, MySQL, Apache
-%w{php php-devel php-mbstring php-mysql mysql-server httpd}.each do |p|
+# PHP, Apache
+%w{php php-devel php-mbstring php-mysql httpd}.each do |p|
     package p do
         action :install
         options "--enablerepo=remi --enablerepo=remi-php55"
     end
 end
+
+# MySQL
+remote_file "/tmp/#{node['mysql']['file_name']}" do
+    source "#{node['mysql']['remote_uri']}"
+end
+bash "install_mysql" do
+    user "root"
+    cwd "/tmp"
+    code <<-EOH
+        tar xf "#{node['mysql']['file_name']}"
+    EOH
+end
+node['mysql']['rpm'].each do |rpm|
+  package rpm[:package_name] do
+    action :install
+    provider Chef::Provider::Package::Rpm
+    source "/tmp/#{rpm[:rpm_file]}"
+  end
+end
+service "mysqld" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
 
 service "iptables" do
   action [:disable, :stop]
